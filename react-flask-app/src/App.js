9e15f4e126eefe8a3c530f09ff94b072e180b40d
favade <fav.adek0ya@gmail.com>
2023-12-05 11:46:47 -0500

    Add login/register and db

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,12 +1,29 @@
-
+import React, { useState } from "react";
+import logo from './logo.svg';
 import './App.css';
+import { Login } from "./screens/Login";
+import { Register } from "./screens/Register";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './screens/Dashboard';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <div>
-      
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
-}
+} 
+
 
 export default App;
+
+
+
